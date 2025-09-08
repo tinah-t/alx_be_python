2b4cc1004a3e56b891d44d2a493e788577649e89
Hana <gerenett@gmail.com>
2025-09-08 21:18:41 +0300

    the word monthly added

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = float(input("Enter your monthly income: "))
-expenses = float(input("Enter your total monthly expenses: "))
-savings = income - expenses
-projected_savings = savings * 12 + (savings * 12 * 0.05)
-print("Your monthly savings are $",savings)
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print("Your monthly savings are $",monthly_savings)
 print("Projected savings after one year, with interest, is: $",projected_savings)
