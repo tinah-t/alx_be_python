8a3dfe37b8aba66e3ddb4baf93fc7ff2af3de7ea
Hana <gerenett@gmail.com>
2025-09-28 20:34:19 +0300

    added the numeric checker

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,28 +10,32 @@ def main():
     while True:
         display_menu()
         choice = input("Enter your choice: ")
-
-        if choice == '1':
-            # Prompt for and add an item
-            item = int(input("What number do you want to add to the list?"))
-            shopping_list.append(item)
-            pass
-        elif choice == '2':
-            item = int(input("What number do you want to remove from the list?"))
-            if item in shopping_list:
-                shopping_list.remove(item)
+        if choice.isnumeric():
+            if choice == '1':
+                # Prompt for and add an item
+                item = input("What number do you want to add to the list?")
+                if item.isnumeric():
+                    shopping_list.append(item)
+                else:
+                    print("Input must be a number")
+                pass
+            elif choice == '2':
+                item = input("What number do you want to remove from the list?")
+                if (item in shopping_list & item.isDigit()) :
+                    shopping_list.remove(item)
+                else:
+                    print("Try again")
+                pass
+            elif choice == '3':
+                print(shopping_list)
+                pass
+            elif choice == '4':
+                print("Goodbye!")
+                break
             else:
-                print("Item is not found")
-            pass
-        elif choice == '3':
-            print(shopping_list)
-            pass
-        elif choice == '4':
-            print("Goodbye!")
-            break
+                print("Invalid choice. Please try again.")
         else:
-            print("Invalid choice. Please try again.")
-
+            print("The choice must be a number")
 
 if __name__ == "__main__":
     main()
