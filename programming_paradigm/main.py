4f94bdf20f8f3042b476bc8eefc9de85dde91d2e
Hana <gerenett@gmail.com>
2025-10-06 16:23:24 +0300

    return statement for the try improved

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -10,7 +10,7 @@ def main():
     denominator = sys.argv[2]
 
     result = safe_divide(numerator, denominator)
-    print(f"The result of the division is {result}")
+    print(result)
 
 if __name__ == "__main__":
     main()
