4f799dffe9e722d8255d28eb8548dbe8c5a12362
Hana <gerenett@gmail.com>
2025-10-06 18:38:46 +0300

    function spellings

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,11 +5,11 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         # Create a new calculator instance before each test
         self.calc = SimpleCalculator()
-    def test_add(self):
+    def test_addition(self):
         result = self.calc.add(5,3)
         self.assertEqual(result,8)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
         self.assertEqual(self.calc.subtract(-2, -2), 0)
