de0e6996eb32464693165841734fe06dabf6ec2b
Hana <gerenett@gmail.com>
2025-10-06 16:18:57 +0300

    try catch impl for division

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,12 @@
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+        return num/den
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Please enter numeric values only."
+
+
+    
