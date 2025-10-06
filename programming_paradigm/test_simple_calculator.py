96d78ec6891b7facf7fc50669e6a0fa09b1ae2d2
Hana <gerenett@gmail.com>
2025-10-06 18:35:05 +0300

    implementing unit test

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,31 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        # Create a new calculator instance before each test
+        self.calc = SimpleCalculator()
+    def test_add(self):
+        result = self.calc.add(5,3)
+        self.assertEqual(result,8)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-2, -2), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+
+    # Test division by zero
+    def test_divide_by_zero(self):
+        self.assertEqual(self.calc.divide(10, 0), None)
+
+if __name__ == '__main__':
+    unittest.main()
