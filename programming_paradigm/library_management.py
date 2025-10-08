4dce36f3635752ee3490172a380d7f83a7d225f0
Hana <gerenett@gmail.com>
2025-10-08 17:09:31 +0300

    spacing

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -18,7 +18,7 @@ class Book:
 
 class Library:
     def __init__(self):
-        self._books=[]
+        self._books = []
     def add_book(self,book):
         self._books.append(book)
     def check_out_book(self,title):
