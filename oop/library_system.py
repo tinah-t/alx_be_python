954c45eb199d519459c91bb05c7436f3983ad38f
Hana <gerenett@gmail.com>
2025-10-11 15:13:44 +0300

    str method to each class

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,14 +2,21 @@ class Book:
     def __init__(self ,title ,author):
         self.title = title
         self.author = author
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 class Library(EBook, PrintBook):
     def __init__(self ):
         self.books = []
@@ -17,10 +24,4 @@ class Library(EBook, PrintBook):
         self.books.append(book)
     def list_books(self):
         for book in self.books:
-            if not hasattr(book, 'file_size') and not hasattr(book, 'page_count') :
-                print(f"Book: {book.title} by {book.author}")
-            elif not hasattr(book, 'page_count') and hasattr(book, 'file_size'):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            else:
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-    
+            print(book)
